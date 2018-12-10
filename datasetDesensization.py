be7e715edfa8e436aecbdd3f363484bcbb3faef1
jiaorui <ruijiao@mail.ustc.edu.cn>
2018-12-10 20:28:49 +0800

    test

--- datasetDesensization.py
+++ datasetDesensization.py
@@ -124,4 +124,8 @@ if __name__=='__main__':
     resultPath="/datapool/workspace/jiaorui/visual_desensitization/result"
 
     DD=datasetDeseneization(datasetPath=datasetPath,resultPath=resultPath)
-    DD.imageInpainting(imageHeight=512,imageWidth=680,checkpointDir="/datapool/workspace/jiaorui/visual_desensitization/model_logs/Places2")    
+    #DD.imageInpainting(imageHeight=512,imageWidth=680,checkpointDir="/datapool/workspace/jiaorui/visual_desensitization/model_logs/Places2")    
+    images=DD.getImageShowMinError(number=2)
+    print(images)
+    images=DD.getImageShowRandom(number=2)
+    print(images)
