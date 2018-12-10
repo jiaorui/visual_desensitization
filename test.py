5ded59883927b03c667be5526e49e61db0a5cb5e
jiaorui <ruijiao@mail.ustc.edu.cn>
2018-12-10 18:49:11 +0800

    a little modify

--- test.py
+++ test.py
@@ -11,6 +11,6 @@ import pickle
 
 data=pickle.load(open("/datapool/workspace/yuanmu/demo_labelset/Stanford40/run_darknet_imgpath2res.pkl","rb"))
 
-test=data['/datapool/workspace/yuanmu/demo_datasets/JPEGImages//looking_through_a_telescope_118.jpg']
+test=data['/datapool/workspace/yuanmu/demo_datasets/JPEGImages/looking_through_a_telescope_118.jpg']
 print(test)
 print(type(test))
